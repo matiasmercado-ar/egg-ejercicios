c4b41a331e0499fe2dc0897dd6e4d6f32368dad4
Matias <matias@matiasmercado.ar>
2021-08-19 07:58:01 -0300

    Ejercicios de coleccion

--- ejercicio4/ejercicio4/src/entidades/Peliculas.java
+++ ejercicio4/ejercicio4/src/entidades/Peliculas.java
@@ -5,10 +5,65 @@
  */
 package entidades;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Mati
  */
 public class Peliculas {
+ 
+    private String titulo;
+    private String director;
+    private int duracion;
+    
+    Scanner keyb = new Scanner (System.in).useDelimiter("\n");
+
+    public Peliculas() {
+    }
+
+    public Peliculas(String titulo, String director, int duracion) {
+        this.titulo = titulo;
+        this.director = director;
+        this.duracion = duracion;
+        
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+    
+    public Peliculas crearPelicula (){
+        System.out.println("Introducir titulo pelicula: ");
+        titulo = keyb.next();
+        
+        System.out.println("Introducir director: ");
+        director = keyb.next();
+        
+        System.out.println("Introducir duracion (horas): ");
+        duracion = keyb.nextInt();
+        
+        return new Peliculas (titulo, director, duracion);
+    }
     
 }
--- ejercicio4/ejercicio4/src/main/Main.java
+++ ejercicio4/ejercicio4/src/main/Main.java
@@ -5,10 +5,53 @@
  */
 package main;
 
+import entidades.Peliculas;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Mati
  */
 public class Main {
+    
+    public static void main(String[] args) {
+        
+        ArrayList<Peliculas> listaPeliculas = new ArrayList ();
+        int finalizar = 0;
+        Scanner keyb = new Scanner (System.in).useDelimiter("\n");
+        
+        
+        do{
+            
+            Peliculas pelicula = new Peliculas ();
+            listaPeliculas.add(pelicula.crearPelicula());
+            
+            System.out.println("Desea agregar una nueva pelicula? (Y/N)");
+            String aux = keyb.next().toUpperCase();
+
+            while (!(aux.equals("N") || aux.equals("Y"))) {
+                System.out.println("Letra incorrecta, ingrese Y o N");
+                aux = keyb.next();
+            }
+            
+            if (aux.equals("N")){
+                finalizar = 1;
+            }
+            
+        }while (finalizar == 0);
+        
+        for(Peliculas i : listaPeliculas){
+            System.out.println("Todas las peluculas"+i.getTitulo());
+        }
+        
+        for (Peliculas i : listaPeliculas){
+            if (i.getDuracion()>1){
+                System.out.println("Peliculas que duran más de 1 hora: "+i.getTitulo());
+            }
+        }
+        
+        
+    }
     
 }
